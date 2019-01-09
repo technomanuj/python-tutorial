367c6e94f0596bd2d54da33764a49bf39d46af73
technomanuj <technomanuj@gmail.com>
2019-01-09 16:07:43 +0530

    Add comments

--- PrintPatterns.py
+++ PrintPatterns.py
@@ -7,7 +7,6 @@ Python’s print() function comes with a parameter called ‘end’.
 By default, the value of this parameter is ‘\n’, i.e. the new line character. 
 You can end a print statement with any character/string using this parameter.
 '''
-
 for istart in range(4):
     while iEnd<5:
        print(ch, end =" ")
