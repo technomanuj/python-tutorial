a328d663fd9f3596965ee7c4aa975d784b454837
technomanuj <technomanuj@gmail.com>
2019-01-09 17:58:14 +0530

    defining functions in python

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,10 +1,22 @@
 # Fibonacci series In Python:
 # the sum of two elements defines the next element
 
+print('write Fibonacci series using while loop ')
 a, b = 0, 1
-while a <= 50:
+while a <= 20:
     print(' The value of a is ', a)
     a, b = b, a+ b
 
+print('write Fibonacci series using functions ')
 
-print('end of Fibonacci series')
+
+def fib(n):    # write Fibonacci series up to n
+    num1, num2 = 0, 1
+    while num1 < n:
+        print(num1, end=' ')
+        num1, num2 = num2, num1 + num2
+    print()
+
+
+fib(50)
+
