ec4ba5a849b10813c1e96b54d3478916ad3786db
technomanuj <technomanuj@gmail.com>
2019-01-09 16:03:12 +0530

    Edit code to print patterns

--- PrintPatterns.py
+++ PrintPatterns.py
@@ -1,13 +1,10 @@
 ch= "#"
-istart = 1
 iEnd = 1
 
-
-while istart<5:
+for istart in range(4):
     while iEnd<5:
        print(ch, end =" ")
        iEnd = iEnd + 1
     iEnd = 1
     print(" ")
-    istart = istart + 1
 
