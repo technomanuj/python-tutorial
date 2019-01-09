e72eb3fd08ad231585f13462e5be08f0b39fadd2
technomanuj <technomanuj@gmail.com>
2019-01-09 13:32:32 +0530

    first commit

---
+++ Fibonacci.py
@@ -0,0 +1,12 @@
+# Fibonacci series:
+# the sum of two elements defines the next
+
+a, b = 0, 1
+while a <= 50:
+    print(' The value of a is ', a)
+    a, b = b, a+ b
+
+if 8>5:
+    print('ww')
+
+print('end of series')
