2469aebbe621ec28894014bc1767469beb02d974
technomanuj <technomanuj@gmail.com>
2019-01-09 15:16:28 +0530

    Edit:Print a specific pattern

--- PrintPatterns.py
+++ PrintPatterns.py
@@ -3,11 +3,11 @@ istart = 1
 iEnd = 1
 
 
-while istart<= 4
-    while iEnd<= 4
-       print(ch," ")
+while istart<5:
+    while iEnd<5:
+       print(ch, end =" ")
        iEnd = iEnd + 1
     iEnd = 1
-    istart = istart +1
+    print(" ")
+    istart = istart + 1
 
-print('End')
