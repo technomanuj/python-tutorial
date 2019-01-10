86e2fa6f149bf58670d32075848ea755357180c3
technomanuj <technomanuj@gmail.com>
2019-01-10 15:48:05 +0530

    Example of arrays in python

---
+++ Array-Example.py
@@ -0,0 +1,14 @@
+# Examples of Arrays
+# When people talk about Python arrays, more often than not, they are talking about Python lists.
+# you will have to use array module to create arrays in python
+
+import array as arr
+
+a = arr.array('d', [1.0, 3.5, 5.5])
+print(a)
+
+'''
+    y = 6
+    print(type(y))
+'''
+
