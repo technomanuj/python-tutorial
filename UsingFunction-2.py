17be5e1d9e1cf0fbc8fbeb3c250a7f718b7a455e
technomanuj <technomanuj@gmail.com>
2019-01-11 17:48:57 +0530

    Import a module that contains all the user defined function that are used in this file

--- UsingFunction-2.py
+++ UsingFunction-2.py
@@ -1,38 +1,8 @@
-# function that return list of name whose length is greater than 5
-def morethanfive(*username):
-    result = []
-    for name in username:
-        if len(name) > 4:
-            result.append(name)
-    return result
-
-
-# function that return count of names whose length is greater than 5
-def name_gt_five_char(*username):
-    # ls = ["true", "true", "false"]
-    # print(ls.count("true"))
-    return [len(name) > 4 for name in username].count(True)
-
-
-# find factorial of a number using loop
-def factorial(num):
-    x = 1
-    while num >= 1:
-        x = x * num
-        num = num - 1
-    return x
-
-
-# find factorial of a number using recursion
-def fact(num):
-    if num == 0:
-        return 1
-
-    return num * fact(num - 1)
-
+# import user defined module
+from mymodule import *
+# import mymodule as mm
 
 neon = 5
-
 print('Factorial of {0} is'.format(neon), fact(neon))
 
 print("Name with maximum five characters", morethanfive("manoj", "anuj", "tanuj", "kaka", "rajkishore"))
---
+++ mymodule.py
@@ -0,0 +1,31 @@
+# function that return list of name whose length is greater than 5
+def morethanfive(*username):
+    result = []
+    for name in username:
+        if len(name) > 4:
+            result.append(name)
+    return result
+
+
+# function that return count of names whose length is greater than 5
+def name_gt_five_char(*username):
+    # ls = ["true", "true", "false"]
+    # print(ls.count("true"))
+    return [len(name) > 4 for name in username].count(True)
+
+
+# find factorial of a number using loop
+def factorial(num):
+    x = 1
+    while num >= 1:
+        x = x * num
+        num = num - 1
+    return x
+
+
+# find factorial of a number using recursion
+def fact(num):
+    if num == 0:
+        return 1
+
+    return num * fact(num - 1)
