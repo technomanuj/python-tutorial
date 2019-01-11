1a93cd4e8d72aa28ba1ba98bc84b4d874c768632
technomanuj <technomanuj@gmail.com>
2019-01-11 15:09:54 +0530

    move user defined function in a new file UsingFunction-2

--- global-local-variable.py
+++ global-local-variable.py
@@ -7,31 +7,7 @@ def something():
     print("Local", a)
     print("Address of a", id(a))
 
-def morethanfive(*username):
-    result = []
-    for name in username:
-        if len(name) > 4:
-            result.append(name)
 
-    return result
-
-
-def countnamelenghtgreaterfive(*username):
-    result = []
-    for name in username:
-        if len(name) > 4:
-            result.append(name)
-    '''
-        ls = ["true", "true", "false"]
-        print(ls.count("true"))
-    '''
-
-    return [len(name) > 4 for name in username].count(True)
-
-
-print(morethanfive("manuj", "anuj", "tanuj", "kaka", "rajkishore"))
-
-print(countnamelenghtgreaterfive("manuj", "anuj", "tanuj", "kaka", "rajkishore"))
 
 something()
 
