36835467fba44de992a9a51f1aa21e5becb44b32
technomanuj <technomanuj@gmail.com>
2019-01-11 15:49:23 +0530

    function to find factorial of a number using loop and also using recursion

---
+++ UsingFunction-2.py
@@ -0,0 +1,39 @@
+# function that return list of name whose length is greater than 5
+def morethanfive(*username):
+    result = []
+    for name in username:
+        if len(name) > 4:
+            result.append(name)
+    return result
+
+# function that return count of names whose length is greater than 5
+def countnamelenghtgreaterfive(*username):
+    # ls = ["true", "true", "false"]
+    # print(ls.count("true"))
+    return [len(name) > 4 for name in username].count(True)
+
+
+# find factorial of a number using loop
+def factorial(num):
+    x = 1
+    while num >= 1:
+        x = x * num
+        num = num - 1
+    return x
+
+
+# find factorial of a number using recursion
+def fact(num):
+    if num == 0:
+        return 1
+
+    return num * fact(num - 1)
+
+
+neon = 5
+
+print('Factorial of {0} is'.format(neon), fact(neon))
+
+print("Name with maximum five characters", morethanfive("manuj", "anuj", "tanuj", "kaka", "rajkishore"))
+
+print("Total name have more than five characters", countnamelenghtgreaterfive("manuj", "anuj", "tanuj", "kaka", "rajkishore"))
